796100bf2e262a390bd98f34b7dba7a849c8bc41
Tommy Str├Âmb├ñck <tommy@anaproit.se>
2021-10-18 13:21:41 +0200

    Updated CosmosDB-settings for FunctionApp

---
+++ Kenkata_Adm_Products/Kenkata_Adm_Products/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kenkata_Adm_Products.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public int CustomerCathegory { get; set; }
+        public int ProductSeason { get; set; }
+        public int ProductCathegory { get; set; }
+        public int ProductBrand { get; set; }
+        public int ProductColor { get; set; }
+        public int ProductSize { get; set; }
+        public int ProductsInStock { get; set; }
+        public int ProductPrice { get; set; }
+
+    }
+}
---
+++ Kenkata_Adm_Products/ProductsDB/Products.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using System.Threading.Tasks;
+
+namespace ProductsDB
+{
+    class Product
+    {
+        [FunctionName("apl2Products")]
+        public static async Task<IActionResult> Run(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
+        [CosmosDB(
+            databaseName:"ProductDB",
+            collectionName:"Products",
+            ConnectionStringSetting = "CosmocDB"
+            )] IEnumerable<Product> questionSet,
+                ILogger log)
+        {
+            log.LogInformation("Data fetched from KenkataProductDB");
+            return new OkObjectResult(questionSet);
+        }
+    }
+}
