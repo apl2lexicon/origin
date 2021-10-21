63548018c903ceb15c31193f5f62fad5a64ab601
Mats Johansson <mats-johansson@telia.com>
2021-10-21 16:35:14 +0200

    Added GetProductsInStock, GetProductsByFilter

---
+++ Kenkata_Adm_Products/ProductsDB/GetProductsByFilter.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using ProductsDB.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductsDB
+{
+    class GetProductsByFilter
+    {
+        [FunctionName("GetProductsByFilter")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "GetProductsByFilter/{Cathegory}/{Color}/{Brand}/{Size}")] HttpRequest req,
+            [CosmosDB(
+                databaseName: "ProductDB",
+                collectionName: "Products",
+                CreateIfNotExists = true,
+                ConnectionStringSetting = "CosmocDB",
+                SqlQuery = "SELECT * FROM products p WHERE p.Cathegory = {Cathegory} AND" +
+                    " p.Color = {Color} AND p.Size = {Size} AND p.Brand = {Brand}"
+            )] IEnumerable<ProductModel> products,
+            ILogger log)
+        {
+            Console.WriteLine("Number of records found: " + products.Count());
+
+            return new OkObjectResult(products);
+        }
+    }
+}
