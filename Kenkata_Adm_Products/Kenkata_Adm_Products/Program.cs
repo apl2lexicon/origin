76aa7416c425701405d49f0f0708f2e896a3be9a
Mats Johansson <mats-johansson@telia.com>
2021-10-25 15:07:41 +0200

    Added new functionality to Program.cs

--- Kenkata_Adm_Products/Kenkata_Adm_Products/Program.cs
+++ Kenkata_Adm_Products/Kenkata_Adm_Products/Program.cs
@@ -108,5 +108,196 @@ namespace Kenkata_Adm_Products
                 Console.WriteLine("Customer " + id + " is updated");
             }
         }
+
+
+        public static void DeleteCustomer(int id)
+        {
+            using (var conn = new SqlConnection(@"Server=tcp:apl2sql.database.windows.net,1433;Initial Catalog=apl2sql;Persist Security Info=False;User ID=apl2sql;Password={your_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"))
+            {
+                conn.Open();
+
+                var model = new CustomerModel();
+
+                using var cmd = new SqlCommand("DELETE FROM Customer c WHERE c.Id = @Id", conn);
+                cmd.Parameters.AddWithValue("@Id", id);
+
+                int rowsAffected = cmd.ExecuteNonQuery();
+
+                if (rowsAffected > 0)
+                {
+                    Console.WriteLine("Customer " + id + " is deleted");
+                }
+            }
+        }
+
+
+        public static object GetOrderById(int id)
+        {
+            using (var conn = new SqlConnection(@"Server=tcp:apl2sql.database.windows.net,1433;Initial Catalog=apl2sql;Persist Security Info=False;User ID=apl2sql;Password={your_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"))
+            {
+                conn.Open();
+
+                using var cmd = new SqlCommand("SELECT * FROM Order o WHERE o.Id = @Id", conn);
+                cmd.Parameters.AddWithValue("@Id", id);
+
+                var result = cmd.ExecuteReader();
+                var order = new OrderModel();
+
+                while (result.Read())
+                {
+                    order.Id = (int)result.GetValue(0);
+                    order.CustomerId = (int)result.GetValue(1);
+                    order.OrderDate = (DateTime)result.GetValue(2);
+                    order.Total = (decimal)result.GetValue(3);
+                    order.PayMethod = (int)result.GetValue(4);
+                    order.Payed = (bool)result.GetValue(5);
+                    order.Canceled = (DateTime)result.GetValue(6);
+                    order.Basket = (bool)result.GetValue(7);
+                    order.Created = (DateTime)result.GetValue(8);
+                }
+
+                return order;
+            }
+        }
+
+        public static void RegisterOrder(OrderModel order)
+        {
+            using (var conn = new SqlConnection(@"Server=tcp:apl2sql.database.windows.net,1433;Initial Catalog=apl2sql;Persist Security Info=False;User ID=apl2sql;Password={your_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"))
+            {
+                conn.Open();
+
+                string sqlString = "INSERT INTO Order (CustomerId, OrderDate, Total, PayMethod, Payed, Canceled, Basket, Created) VALUES (@CustomerId, @OrderDate, @Total, @PayMethod, @Payed, @Canceled, @Basket, @Created)";
+
+                using var cmd = new SqlCommand(sqlString, conn);
+                cmd.Parameters.AddWithValue("@CustomerId", order.CustomerId);
+                cmd.Parameters.AddWithValue("@OrderDate", order.OrderDate);
+                cmd.Parameters.AddWithValue("@Total", order.Total);
+                cmd.Parameters.AddWithValue("@PayMethod", order.PayMethod);
+                cmd.Parameters.AddWithValue("@Payed", order.Payed);
+                cmd.Parameters.AddWithValue("@Canceled", order.Canceled);
+                cmd.Parameters.AddWithValue("@Basket", order.Basket);
+                cmd.Parameters.AddWithValue("@Created", order.Created);
+
+                var id = (int)cmd.ExecuteScalar();
+
+                Console.WriteLine("ID: " + id);
+                Console.WriteLine("Customer Created");
+            }
+        }
+
+        public static void DeleteOrder(int id)
+        {
+            using (var conn = new SqlConnection(@"Server=tcp:apl2sql.database.windows.net,1433;Initial Catalog=apl2sql;Persist Security Info=False;User ID=apl2sql;Password={your_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"))
+            {
+                conn.Open();
+
+                using var cmd = new SqlCommand("DELETE FROM Order o WHERE o.Id = @Id", conn);
+                cmd.Parameters.AddWithValue("@Id", id);
+
+                int rowsAffected = cmd.ExecuteNonQuery();
+
+                if (rowsAffected > 0)
+                {
+                    Console.WriteLine("Order + " + id + " is deleted.");
+                }
+            }
+        }
+
+
+        public static object GetOrderLinesById(int id)
+        {
+            using (var conn = new SqlConnection(@"Server=tcp:apl2sql.database.windows.net,1433;Initial Catalog=apl2sql;Persist Security Info=False;User ID=apl2sql;Password={your_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"))
+            {
+                conn.Open();
+
+                var model = new OrderLineModel();
+
+                using var cmd = new SqlCommand("SELECT * FROM OrderLine o WHERE o.OrderId = @Id", conn);
+                cmd.Parameters.AddWithValue("@Id", id);
+
+                var result = cmd.ExecuteReader();
+                var orderLine = new OrderLineModel();
+
+                if (result.HasRows)
+                {
+                    while (result.Read())
+                    {
+                        orderLine.OrderLineNr = (int)result.GetValue(0);
+                        orderLine.OrderId = (int)result.GetValue(1);
+                        orderLine.ProdId = (int)result.GetValue(2);
+                        orderLine.Quantity = (int)result.GetValue(3);
+                        orderLine.APrice = (int)result.GetValue(4);
+                        orderLine.Created = (DateTime)result.GetValue(5);
+                    }
+
+                    Console.WriteLine("Order lines found");
+                }
+                else
+                {
+                    Console.WriteLine("No Order lines found");
+                }
+
+                return orderLine;
+            }
+        }
+
+
+        public static object GetOrderLineByLineNo(int id, int lineNumber)
+        {
+            using (var conn = new SqlConnection(@"Server=tcp:apl2sql.database.windows.net,1433;Initial Catalog=apl2sql;Persist Security Info=False;User ID=apl2sql;Password={your_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"))
+            {
+                conn.Open();
+
+                using var cmd = new SqlCommand("SELECT * FROM OrderLine o WHERE o.OrderLineNr = @LineNo AND o.OrderId = @Id", conn);
+                cmd.Parameters.AddWithValue("@LineNo", lineNumber);
+                cmd.Parameters.AddWithValue("@Id", id);
+
+                var result = cmd.ExecuteReader();
+                var orderLine = new OrderLineModel();
+
+                if(result.HasRows)
+                {
+                    while (result.Read())
+                    {
+                        orderLine.OrderLineNr = (int)result.GetValue(0);
+                        orderLine.OrderId = (int)result.GetValue(1);
+                        orderLine.ProdId = (int)result.GetValue(2);
+                        orderLine.Quantity = (int)result.GetValue(3);
+                        orderLine.APrice = (int)result.GetValue(4);
+                        orderLine.Created = (DateTime)result.GetValue(5);
+                    }
+
+                    Console.WriteLine("Order lines found");
+                }
+                else
+                {
+                    Console.WriteLine("No Order lines found");
+                }
+
+                return orderLine;
+            }
+        }
+
+
+        public static void RegisterOrderLine(OrderLineModel orderLine)
+        {
+            using (var conn = new SqlConnection(@"Server=tcp:apl2sql.database.windows.net,1433;Initial Catalog=apl2sql;Persist Security Info=False;User ID=apl2sql;Password={your_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"))
+            {
+                conn.Open();
+
+                var model = new OrderLineModel();
+
+                using var cmd = new SqlCommand("INSERT INTE OrderLine (OrderLineNr, OrderId, ProdId, Quantity, APrice, Created) VALUES (@OrderLineNr, @OrderId, @ProdId, @Quantity, @APrice, @Created)", conn);
+                cmd.Parameters.AddWithValue("@OrderLineNr", orderLine.OrderLineNr);
+                cmd.Parameters.AddWithValue("@OrderId", orderLine.OrderId);
+                cmd.Parameters.AddWithValue("@ProdId", orderLine.ProdId);
+                cmd.Parameters.AddWithValue("@Quantity", orderLine.Quantity);
+                cmd.Parameters.AddWithValue("@APrice", orderLine.APrice);
+                cmd.Parameters.AddWithValue("@Created", orderLine.Created);
+            }
+        }
+
+
+
     }
 }
