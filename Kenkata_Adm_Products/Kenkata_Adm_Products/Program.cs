aa521b6c058ea64a104e75357369db32968af646
Mats Johansson <mats-johansson@telia.com>
2021-10-25 09:19:46 +0200

    Added functions RegisterCustomer and UpdateCustomer to Program.cs

--- Kenkata_Adm_Products/Kenkata_Adm_Products/Program.cs
+++ Kenkata_Adm_Products/Kenkata_Adm_Products/Program.cs
@@ -57,36 +57,55 @@ namespace Kenkata_Adm_Products
         }
 
 
-        public static void CreateCustomer(CustomerModel customer)
+        public static void RegisterCustomer(CustomerModel customer)
         {
             using (var conn = new SqlConnection(@"Server=tcp:apl2sql.database.windows.net,1433;Initial Catalog=apl2sql;Persist Security Info=False;User ID=apl2sql;Password={your_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"))
             {
                 conn.Open();
-                var model = new CustomerModel();
-
-                //model.Id = customer.Id;
-                model.FirstName = customer.FirstName;
-                model.LastName = customer.LastName;
-                model.Address = customer.Address;
-                model.Address2 = customer.Address2;
-                model.AreaCode = customer.AreaCode;
-                model.City = customer.City;
-                model.Email = customer.Email;
-                model.ContactInfo = customer.ContactInfo;
-                model.Created = customer.Created;
 
                 using var cmd = new SqlCommand("INSERT INTO Customer (FirstName, LastName, Address, Address2, AreaCode, City, Email, ContactInfo, Created) OUTPUT Inserted.Id VALUES (@FirstName, @LastName, @Address, @Address2, @AreaCode, @City, @Email, @ContactInfo, @Created", conn);
-                cmd.Parameters.AddWithValue("@FirstName", model.FirstName);
-                cmd.Parameters.AddWithValue("@LastName", model.LastName);
-                cmd.Parameters.AddWithValue("@Address", model.Address);
-                cmd.Parameters.AddWithValue("@Address2", model.Address2;
-                cmd.Parameters.AddWithValue("@AreaCode", model.AreaCode);
-                cmd.Parameters.AddWithValue("@City", model.City);
-                cmd.Parameters.AddWithValue("@Email", model.Email);
-                cmd.Parameters.AddWithValue("@ContactInfo", model.ContactInfo);
-                cmd.Parameters.AddWithValue("@Created", model.Created);
+                cmd.Parameters.AddWithValue("@FirstName", customer.FirstName);
+                cmd.Parameters.AddWithValue("@LastName", customer.LastName);
+                cmd.Parameters.AddWithValue("@Address", customer.Address);
+                cmd.Parameters.AddWithValue("@Address2", customer.Address2);
+                cmd.Parameters.AddWithValue("@AreaCode", customer.AreaCode);
+                cmd.Parameters.AddWithValue("@City", customer.City);
+                cmd.Parameters.AddWithValue("@Email", customer.Email);
+                cmd.Parameters.AddWithValue("@ContactInfo", customer.ContactInfo);
+                cmd.Parameters.AddWithValue("@Created", customer.Created);
+
+                var id = (int)cmd.ExecuteScalar();
+
+                Console.WriteLine("ID: " + id);
+                Console.WriteLine("Customer Created");
+            }
+        }
 
 
+        public static void UpdateCustomer(CustomerModel customer)
+        {
+            using (var conn = new SqlConnection(@"Server=tcp:apl2sql.database.windows.net,1433;Initial Catalog=apl2sql;Persist Security Info=False;User ID=apl2sql;Password={your_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"))
+            {
+                conn.Open();
+
+                int id = customer.Id;
+                string sqlString = "UPDATE customer c SET c.FirstName = @FirstName, c.LastName = @LastName, c.Address = @Address, c.Address2 = @Address2, c.AreaCode = @AreaCode, c.City = @City, c.Email = @Email, c.ContactInfo = @ContactInfo, c.Created = @Created WHERE c.Id = @Id";
+
+                using var cmd = new SqlCommand(sqlString, conn);
+                cmd.Parameters.AddWithValue("@Id", id);
+                cmd.Parameters.AddWithValue("@FirstName", customer.FirstName);
+                cmd.Parameters.AddWithValue("@LastName", customer.LastName);
+                cmd.Parameters.AddWithValue("@Address", customer.Address);
+                cmd.Parameters.AddWithValue("@Address2", customer.Address2);
+                cmd.Parameters.AddWithValue("@AreaCode", customer.AreaCode);
+                cmd.Parameters.AddWithValue("@City", customer.City);
+                cmd.Parameters.AddWithValue("@Email", customer.Email);
+                cmd.Parameters.AddWithValue("@ContactInfo", customer.ContactInfo);
+                cmd.Parameters.AddWithValue("@Created", customer.Created);
+
+                cmd.ExecuteNonQuery();
+
+                Console.WriteLine("Customer " + id + " is updated");
             }
         }
     }
